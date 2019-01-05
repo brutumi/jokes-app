250350f1fdbe2eb8604ead6bc4a594254a6738a3
brutumi <jovanj9730@gmail.com>
2019-01-05 12:22:36 +0100

    XML Configuration

--- src/main/java/com/netchronic/jokesapp/JokesAppApplication.java
+++ src/main/java/com/netchronic/jokesapp/JokesAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesAppApplication {
 
     public static void main(String[] args) {
